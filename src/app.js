8c3b765150e200dd21c3c932916a8767651991b4
NevenaVasilevska-TomTom <nevena.vasilievska@tomtom.com>
2023-08-22 14:07:54 +0200

    closing

--- src/app.js
+++ src/app.js
@@ -36,10 +36,20 @@ END_ICON.options.shadowSize = [0,0]
 
 const { latLng } = require("leaflet");
 var data = require("../data/results_processed.json");
+var highlighted = []
 
 // console.log(data.length);
 // console.log(data[0].pointsInTrace);
 // console.log(data[0].pointsInTrace[0]);
+
+
+var map = L.map("map").setView([48.95293, 8.91368], 13);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+var myDiv = document.getElementById("info");
 
 data.forEach((d, i) => {
   d.index = i;  // trip_id
@@ -53,62 +63,123 @@ data.forEach((d, i) => {
   d.points_info = d.pointsInTrace.map((p,i) => 
     [p.index, p.charge, p.rounded_timestamp]
   );
+  d.line = L.hotline(d.latLngs, {
+    min: 0,
+    max: 37.8,
+    palette: NORMAL_PALETTE,
+  })
+    //.bindPopup(`Route ${d.index} (${d.minCharge}, ${d.maxCharge})`)
+    .on(
+      "click",
+      () => highlight(d),
+    ).addTo(map);
 });
-
-var map = L.map("map").setView([48.95293, 8.91368], 13);
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
-
-
-// add someway to edit
-function editPoint(d, point) {
-  d.list_items = document.getElementById(d.index).getElementsByTagName("li");
-  for (let item of d.list_items){
-    item.classList.remove('hint');
-  }
-  d.list_items[point].classList.add('hint')
-}
 
 // Displaying markers for every point seems to slow down rendering
 // Highlight a route => dont slow down browser
-function removeH(d){
+function remove_highlight(d){
   console.log(d)
-  if (this.highlighted) {
-    this.highlighted.chargeMarkersLayer.remove();
-    this.highlighted.d.line.setStyle({
-      outlineWidth: 1,
-      palette: NORMAL_PALETTE,
-    });
+  if(typeof d != 'object'){
+    d = highlighted[highlighted.map(e => e.index).indexOf(d)]
   }
+  let index = highlighted.map(e => e.index).indexOf(d.index);
+  console.log(index);
+  console.log(d);
+  d.chargeMarkersLayer.remove();
+  d.line.setStyle({
+    outlineWidth: 1,
+    palette: NORMAL_PALETTE,
+  });
+  d.line.bringToFront();
+  highlighted.splice(index, 1);
+  console.log(d)
+  console.log(highlighted)
   myDiv.removeChild(document.getElementById(d.index));
 }
+
 function highlight(d) {
-  if (this.highlighted) {
-    this.highlighted.chargeMarkersLayer.remove();
-    this.highlighted.d.line.setStyle({
-      outlineWidth: 1,
-      palette: NORMAL_PALETTE,
-    });
+  console.log(highlighted)
+  if(highlighted.map(e => e.index).indexOf(d.index) != -1){
+    remove_highlight(d);
+    return;
   }
+  if(highlighted.length == 2){
+    remove_highlight(highlighted[0]);
+  }
+  console.log(highlighted)
+
   d.line.setStyle({ outlineWidth: 3, palette: HIGHLIGHTED_PALETTE });
   d.line.bringToFront();
   const markers = d.latLngs.map((latLng, i) =>
     L.marker(latLng, {icon: ICON}).bindPopup(`Charge ${d.pointsInTrace[i].charge} Point ${i}`).on("click",
       () => editPoint(d, i)
     )
-    //L.marker(latLng, {icon: ICON}).bindPopup(`Charge ${d.pointsInTrace[i].charge} Point ${i}`)
-    );
+  );
   if (markers.length) {
     markers[0].setIcon(START_ICON);
     markers[markers.length - 1].setIcon(END_ICON);
   }
-  this.highlighted = {
-    d,
-    chargeMarkersLayer: L.featureGroup(markers).addTo(map),
-  };
-  showInfo(this);
+  d.chargeMarkersLayer = L.featureGroup(markers).addTo(map) 
+  
+  highlighted.push(d)
+  console.log(highlighted)
+  showInfo(d);
+}
+
+function showInfo(d) {
+  var innerHtml = "none";
+  var inner_data = "";
+  d.points_info.forEach( point => {
+    inner_data+= `
+      <li class="point_info"> 
+        <span>id: ${point[0].toString()} | SOC: ${point[1].toString()} | timestamp: ${point[2].toString()} </span>
+      </li>`
+  });
+  innerHtml = `
+  <div id=${d.index} class="track_info">
+    <div class="close" onclick="remove_highlight(${d.index})">&#10005;</div>
+    <h3> Route id: ${d.index}</h3> 
+    <p>${d.latLngs.length} points | ${d.index}kWh | ${d.index}m</p>
+    <ul class="slist">
+      ${inner_data}
+    </ul>
+  </div>
+  `;
+  myDiv.innerHTML += innerHtml;
+  d.list_items = document.getElementById(d.index).getElementsByTagName("li"), current = null;
+  for (let i of d.list_items) {
+    i.draggable = true;
+    i.ondragstart = e => {
+      current = i;
+      for (let it of d.list_items) {
+        if (it != current) { it.classList.add("hint"); }
+      }
+    };
+    i.ondragenter = e => {
+      if (i != current) { i.classList.add("active"); }
+    };
+    i.ondragleave = () => i.classList.remove("active");
+    i.ondragend = () => { for (let it of d.list_items) {
+        it.classList.remove("hint");
+        it.classList.remove("active");
+    }};
+    i.ondragover = e => e.preventDefault();
+    i.ondrop = e => {
+      e.preventDefault();
+      if (i != current) {
+        let currentpos = 0, droppedpos = 0;
+        for (let it=0; it<d.list_items.length; it++) {
+          if (current == d.list_items[it]) { currentpos = it; }
+          if (i == d.list_items[it]) { droppedpos = it; }
+        }
+        if (currentpos < droppedpos) {
+          i.parentNode.insertBefore(current, i.nextSibling);
+        } else {
+          i.parentNode.insertBefore(current, i);
+        }
+      }
+    };
+  }
 }
 
 function movePoint(track_id, point_id){
@@ -125,86 +196,17 @@ function movePoint(track_id, point_id){
   new_track.latLngs.push(point_latlon)
 }
 
-window.movePoint = movePoint;
-window.removeH = removeH;
-
-var myDiv = document.getElementById("info");
-function showInfo(d) {
-  var innerHtml = "none";
-  // console.log(globals.highlighted);
-  // console.log(globals.highlighted.d);
-  var inner_data = "";
-  if (d.highlighted) {
-    //console.log(globals.highlighted.d.infos)
-    d.highlighted.d.points_info.forEach( point => {
-      inner_data+= `
-        <li class="point_info"> 
-          <span>id: ${point[0].toString()} | SOC: ${point[1].toString()} | timestamp: ${point[2].toString()} </span>
-        </li>`
-    });
-    innerHtml = `
-    <div id=${d.highlighted.d.index} class="track_info">
-      <div class="close" onclick="removeH(highlighted.d)">&#10005;</div>
-      <h3> Route id: ${d.highlighted.d.index}</h3> 
-      <p>${d.highlighted.d.latLngs.length} points | ${d.highlighted.d.index}kWh | ${d.highlighted.d.index}m</p>
-      <ul class="slist">
-        ${inner_data}
-      </ul>
-    </div>
-    `;
-    myDiv.innerHTML = innerHtml;
-
-    d.list_items = document.getElementById(d.highlighted.d.index).getElementsByTagName("li"), current = null;
-    for (let i of d.list_items) {
-      i.draggable = true;
-      i.ondragstart = e => {
-        current = i;
-        for (let it of d.list_items) {
-          if (it != current) { it.classList.add("hint"); }
-        }
-      };
-      i.ondragenter = e => {
-        if (i != current) { i.classList.add("active"); }
-      };
-      i.ondragleave = () => i.classList.remove("active");
-      i.ondragend = () => { for (let it of d.list_items) {
-          it.classList.remove("hint");
-          it.classList.remove("active");
-      }};
-      i.ondragover = e => e.preventDefault();
-      i.ondrop = e => {
-        e.preventDefault();
-        if (i != current) {
-          let currentpos = 0, droppedpos = 0;
-          for (let it=0; it<d.list_items.length; it++) {
-            if (current == d.list_items[it]) { currentpos = it; }
-            if (i == d.list_items[it]) { droppedpos = it; }
-          }
-          if (currentpos < droppedpos) {
-            i.parentNode.insertBefore(current, i.nextSibling);
-          } else {
-            i.parentNode.insertBefore(current, i);
-          }
-        }
-      };
-    }
+// add someway to edit
+function editPoint(d, point) {
+  d.list_items = document.getElementById(d.index).getElementsByTagName("li");
+  for (let item of d.list_items){
+    item.classList.remove('hint');
   }
-  
+  d.list_items[point].classList.add('hint')
 }
 
 
-data.map((d) => {
-  // d.line = L.polyline(d.latLngs).bindPopup(`Route ${d.index}`).on(
-  //   "click",
-  //   () => highlight(d),
-  // ).addTo(map);
-  d.line = L.hotline(d.latLngs, {
-    min: 0,
-    max: 37.8,
-    palette: NORMAL_PALETTE,
-  })
-    .bindPopup(`Route ${d.index} (${d.minCharge}, ${d.maxCharge})`).on(
-      "click",
-      () => highlight(d),
-    ).addTo(map);
-});
+
+
+window.movePoint = movePoint;
+window.remove_highlight = remove_highlight;
